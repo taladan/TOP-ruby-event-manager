17e8a285208b2b23ac0870358ea5cb632b6b11ab
D.J. Crosby <taladan@gmail.com>
2023-04-01 16:54:07 -0400

    Create method to obtain legislators information
    Use google-api-client gem to connect to civics
    api and retrieve legislator info, per zipcode.

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,8 +1,24 @@
 require "csv"
 require "google/apis/civicinfo_v2"
 
-civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
-civic_info.key = "AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw"
+def legislators_by_zipcode(zip)
+  civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
+  civic_info.key = "AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw"
+
+  begin
+    legislators =
+      civic_info.representative_info_by_address(
+        address: zip,
+        levels: "country",
+        roles: %w[legislatorUpperBody legislatorLowerBody],
+      )
+    legislators = legislators.officials
+    legislator_names = legislators.map(&:name)
+    legislators_string = legislator_names.join(", ")
+  rescue StandardError
+    "You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials"
+  end
+end
 
 def clean_zipcode(zipcode)
   # if zipcode.nil?
@@ -25,19 +41,7 @@ contents.each do |row|
 
   zipcode = clean_zipcode(row[:zipcode])
 
-  begin
-    legislators =
-      civic_info.representative_info_by_address(
-        address: zipcode,
-        levels: "country",
-        roles: %w[legislatorUpperBody legislatorLowerBody],
-      )
-    legislators = legislators.officials
-    legislator_names = legislators.map(&:name)
-    legislators_string = legislator_names.join(", ")
-  rescue StandardError
-    "You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials"
-  end
+  legislators = legislators_by_zipcode(zipcode)
 
-  puts "#{name} #{zipcode} #{legislators_string}"
+  puts "#{name} #{zipcode} #{legislators}"
 end
