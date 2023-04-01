47231a76a30eea5303854a8544b37304e0c705f6
D.J. Crosby <taladan@gmail.com>
2023-04-01 07:56:57 -0400

    Initial commit
    Working through programming along with the exercise in TOP at:
    https://www.theodinproject.com/lessons/ruby-event-manager

---
+++ lib/event_manager.rb
@@ -0,0 +1,43 @@
+require "csv"
+require "google/apis/civicinfo_v2"
+
+civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
+civic_info.key = "AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw"
+
+def clean_zipcode(zipcode)
+  # if zipcode.nil?
+  #   zipcode = "00000"
+  # elsif zipcode.length < 5
+  #   zipcode = zipcode.rjust(5, "0")
+  # elsif zipcode.length > 5
+  #   zipcode = zipcode[0..4]
+  # end
+  zipcode.to_s.rjust(5, "0")[0..4]
+end
+
+puts "Event Manager Initialized!"
+
+contents =
+  CSV.open("event_attendees.csv", headers: true, header_converters: :symbol)
+
+contents.each do |row|
+  name = row[:first_name]
+
+  zipcode = clean_zipcode(row[:zipcode])
+
+  begin
+    legislators =
+      civic_info.representative_info_by_address(
+        address: zipcode,
+        levels: "country",
+        roles: %w[legislatorUpperBody legislatorLowerBody],
+      )
+    legislators = legislators.officials
+    legislator_names = legislators.map(&:name)
+    legislators_string = legislator_names.join(", ")
+  rescue StandardError
+    "You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials"
+  end
+
+  puts "#{name} #{zipcode} #{legislators_string}"
+end
